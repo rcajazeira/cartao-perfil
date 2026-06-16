9578346d9d8b7bc5b3477dbc1b9001f6e6f611d1
rcajazeira <rcajazeira@gmail.com>
2026-06-16 16:48:34 -0300

    cartão de perfil primeiro commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btnMudar');
+const nome = document.getElementById('nome');
+
+btn.addEventListener('click', () => {
+    alert('Olá! Este é meu cartão de perfil!');
+})
+
+
+
